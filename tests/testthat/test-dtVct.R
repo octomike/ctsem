7021cf5012503fec89e182d96d95efa25431e5ba
Michael Krause <krause@mpib-berlin.mpg.de>
2019-10-01 15:53:47 +0200

    Tests: lintr-ed

--- tests/testthat/test-dtVct.R
+++ tests/testthat/test-dtVct.R
@@ -1,181 +1,187 @@
-if(identical(Sys.getenv("NOT_CRAN"), "true") & .Machine$sizeof.pointer != 4){
+if (identical(Sys.getenv("NOT_CRAN"), "true") & .Machine$sizeof.pointer != 4){
   # Sys.setenv(NOT_CRAN='true')
 
   set.seed(1)
-  
+
   context("dtVct_lVnl")
-  
+
   test_that("dtVct_CINTheterogeneity", {
     set.seed(1)
-    s=list()
-    nsubjects=200
-    Tpoints=15
-    parsd=1.4
-    parmu= -10.4
-    dt=1
-    par= (rnorm(nsubjects,parmu,parsd))
+    s <- list()
+    nsubjects <- 200
+    Tpoints <- 15
+    parsd <- 1.4
+    parmu <- -10.4
+    dt <- 1
+    par <- (rnorm(nsubjects, parmu, parsd))
     mean(par)
     sd(par)
-    
-    for(subi in 1:nsubjects){
-      gm=ctModel(LAMBDA=diag(1), Tpoints=Tpoints, DRIFT=matrix(-1),T0MEANS = matrix(4), 
-        CINT=matrix(par[subi]),DIFFUSION=matrix(2),
-        T0VAR=matrix(2), MANIFESTVAR=matrix(.8))
-      d=suppressMessages(ctGenerate(gm,n.subjects = 1,burnin = 3,wide = FALSE,dtmean = dt))
-      if(subi==1) dat=cbind(subi,d) else dat=rbind(dat,cbind(subi,d))
+
+    for (subi in 1:nsubjects) {
+      gm <- ctModel(LAMBDA = diag(1), Tpoints = Tpoints, DRIFT = matrix(-1),
+                    T0MEANS = matrix(4), CINT = matrix(par[subi]),
+                    DIFFUSION = matrix(2), T0VAR = matrix(2),
+                    MANIFESTVAR = matrix(.8))
+      d <- suppressMessages(ctGenerate(gm, n.subjects = 1, burnin = 3,
+                                       wide = FALSE, dtmean = dt))
+      if (subi == 1) dat <- cbind(subi, d) else dat <- rbind(dat, cbind(subi, d))
     }
-    
-    colnames(dat)[1]='id'
-    
-    cm <- ctModel(LAMBDA=diag(1), type='stanct',
-      CINT=matrix('cint'),
-      MANIFESTMEANS = matrix(0))
-    
-    
+
+    colnames(dat)[1] <- "id"
+
+    cm <- ctModel(LAMBDA = diag(1), type = "stanct", CINT = matrix("cint"),
+                  MANIFESTMEANS = matrix(0))
+
+
     cm$pars$indvarying <- FALSE
-    cm$pars$indvarying[cm$pars$matrix %in% c('CINT','T0MEANS')] <- TRUE
-    
-    dm <- ctModel(LAMBDA=diag(1), type='standt',
-      CINT=matrix('cint'),
-      MANIFESTMEANS = matrix(0))
-    
+    cm$pars$indvarying[cm$pars$matrix %in% c("CINT", "T0MEANS")] <- TRUE
+
+    dm <- ctModel(LAMBDA = diag(1), type = "standt", CINT = matrix("cint"),
+                  MANIFESTMEANS = matrix(0))
+
     dm$pars$indvarying <- FALSE
-    dm$pars$indvarying[dm$pars$matrix %in% c('CINT','T0MEANS')] <- TRUE
-    
-    
-    for(m in c('cm','dm')){
+    dm$pars$indvarying[dm$pars$matrix %in% c("CINT", "T0MEANS")] <- TRUE
+
+
+    for (m in c("cm", "dm")) {
       argslist <- list(
-        ml=list(datalong = dat,ctstanmodel = get(m),optimize=TRUE, nlcontrol=list(),
-        verbose=0,optimcontrol=list(plotsgd=F,estonly=F,stochastic=F),savescores = F,nopriors=T)
-        #, mlis=list(datalong = dat,ctstanmodel = get(m),optimize=TRUE, nlcontrol=list(Jstep=1e-6), 
+        ml = list(datalong = dat, ctstanmodel = get(m), optimize = TRUE,
+                  nlcontrol = list(), verbose = 0,
+                  optimcontrol = list(plotsgd = F, estonly = F, stochastic = F),
+                  savescores = F, nopriors = T)
+        #, mlis=list(datalong = dat,ctstanmodel = get(m),optimize=TRUE, nlcontrol=list(Jstep=1e-6),
         #   verbose=0,optimcontrol=list(plotsgd=F,estonly=F,isloops=1,stochastic=F),savescores = F,nopriors=T)
         # ,mapis=list(datalong = dat,ctstanmodel = get(m),optimize=TRUE, nlcontrol=list(Jstep=1e-6),
         # verbose=0,optimcontrol=list(plotsgd=F,estonly=F,isloops=1,stochastic=F),savescores = F,nopriors=F)
-        # ,hmcintoverpop=list(datalong = dat,ctstanmodel = get(m),optimize=F,iter=500,chains=3, nlcontrol=list(Jstep=1e-6), 
+        # ,hmcintoverpop=list(datalong = dat,ctstanmodel = get(m),optimize=F,iter=500,chains=3, nlcontrol=list(Jstep=1e-6),
         #   verbose=0,optimcontrol=list(plotsgd=F,estonly=F,stochastic=F),savescores = F,nopriors=F,intoverpop=T),
         # hmc=list(datalong = dat,ctstanmodel = get(m),optimize=F,iter=500,chains=3, nlcontrol=list(),
           # verbose=0,optimcontrol=list(plotsgd=F,estonly=F,stochastic=F),savescores = F,nopriors=F,control=list(max_treedepth=7))
       )
-      
-      
-      for(argi in names(argslist)){
-        f = do.call(ctStanFit,argslist[[argi]])
-        if(is.null(s[[argi]])) s[[argi]] = list()
-        s[[argi]][[m]] <- summary(f,parmatrices=TRUE)
+
+
+      for (argi in names(argslist)) {
+        f <- do.call(ctStanFit, argslist[[argi]])
+        if (is.null(s[[argi]])) s[[argi]] <- list()
+        s[[argi]][[m]] <- summary(f, parmatrices = TRUE)
       }
     }
-    
-    dtpars=lapply(s, function(argi) {
-      ct=argi$cm$parmatrices[c(grep('(^dt|^asym)',rownames(argi$cm$parmatrices)),
-        which(rownames(argi$cm$parmatrices) %in% c('MANIFESTVAR','LAMBDA','T0MEANS','T0VAR'))),c('Mean','Sd')]
-      dt=argi$dm$parmatrices[c(grep('(^asym)',rownames(argi$dm$parmatrices)),
-        which(rownames(argi$dm$parmatrices) %in% c('DRIFT','CINT','DIFFUSIONcov','MANIFESTVAR','LAMBDA','T0MEANS','T0VAR'))),c('Mean','Sd')]
-      rownames(dt)[rownames(dt) %in% c('DRIFT','CINT','DIFFUSIONcov')] <- paste0('dt',rownames(dt)[rownames(dt) %in% c('DRIFT','CINT','DIFFUSIONcov')])
-      rownames(dt)[rownames(dt) %in% c('dtDIFFUSIONcov')] <- 'dtDIFFUSION'
-      list(ct=ct,dt=dt)
-    }
-    )
-    dtpars=unlist(dtpars,recursive = FALSE)
-    dtpars=lapply(dtpars,function(x) x[order(rownames(x)),])
-    
-    for(ri in 1:nrow(dtpars$ml.ct)){
-      for(ci in 1:ncol(dtpars$ml.ct)){
-        par=sapply(dtpars, function(y) y[ri,ci])
-        for(dimi in 2:length(par)){
-          expect_equivalent(par[dimi],par[dimi-1],tol=ifelse(ci==1,1e-1,5e-1))
+
+    dtpars <- lapply(s, function(argi) {
+                         ct <- argi$cm$parmatrices[c(grep("(^dt|^asym)", rownames(argi$cm$parmatrices)),
+                                                     which(rownames(argi$cm$parmatrices) %in%
+                                                           c("MANIFESTVAR", "LAMBDA", "T0MEANS", "T0VAR"))
+                                                     ), c("Mean", "Sd")]
+                         dt <- argi$dm$parmatrices[c(grep("(^asym)", rownames(argi$dm$parmatrices)),
+                                                     which(rownames(argi$dm$parmatrices) %in%
+                                                           c("DRIFT", "CINT", "DIFFUSIONcov", "MANIFESTVAR",
+                                                             "LAMBDA", "T0MEANS", "T0VAR"))),
+                                                   c("Mean", "Sd")]
+                         t <- paste0("dt", rownames(dt)[rownames(dt) %in% c("DRIFT", "CINT", "DIFFUSIONcov")])
+                         rownames(dt)[rownames(dt) %in% c("DRIFT", "CINT", "DIFFUSIONcov")] <- t
+                         rownames(dt)[rownames(dt) %in% c("dtDIFFUSIONcov")] <- "dtDIFFUSION"
+                         list(ct = ct, dt = dt)
+    })
+    dtpars <- unlist(dtpars, recursive = FALSE)
+    dtpars <- lapply(dtpars, function(x) x[order(rownames(x)), ])
+
+    for (ri in 1:nrow(dtpars$ml.ct)) {
+      for (ci in 1:ncol(dtpars$ml.ct)) {
+        par <- sapply(dtpars, function(y) y[ri, ci])
+        for (dimi in 2:length(par)) {
+          expect_equivalent(par[dimi], par[dimi - 1], tol = ifelse(ci == 1, 1e-1, 5e-1))
         }
       }}
-    
-    
-    ll=unlist(lapply(s, function(argi) lapply(argi, function(m) m$logprob)))
-    
-    for(dimi in 2:length(ll)){
-      expect_equivalent(ll[dimi],ll[dimi-1],tol=1e-3)
+
+
+    ll <- unlist(lapply(s, function(argi) lapply(argi, function(m) m$logprob)))
+
+    for (dimi in 2:length(ll)) {
+      expect_equivalent(ll[dimi], ll[dimi - 1], tol = 1e-3)
     }
-    
-    
+
+
   }) #end cint heterogeneity
-    
-    
-    test_that("dtVct_noheterogeneity", {
-      set.seed(1)
-      s=list()
-      nsubjects=100
-      Tpoints=10
-      parsd=0
-      parmu= -1.4
-      dt=1
-      par= (rnorm(nsubjects,parmu,parsd))
-      mean(par)
-      sd(par)
-      
-      for(subi in 1:nsubjects){
-        gm=ctModel(LAMBDA=diag(1), Tpoints=Tpoints, DRIFT=matrix(-.5),T0MEANS = matrix(4), 
-          CINT=matrix(par[subi]),DIFFUSION=matrix(2),
-          T0VAR=matrix(2), MANIFESTVAR=matrix(2))
-        d=suppressMessages(ctGenerate(gm,n.subjects = 1,burnin = 10,wide = FALSE,dtmean = dt))
-        if(subi==1) dat=cbind(subi,d) else dat=rbind(dat,cbind(subi,d))
+
+
+  test_that("dtVct_noheterogeneity", {
+    set.seed(1)
+    s <- list()
+    nsubjects <- 100
+    Tpoints <- 10
+    parsd <- 0
+    parmu <- -1.4
+    dt <- 1
+    par <- (rnorm(nsubjects, parmu, parsd))
+    mean(par)
+    sd(par)
+
+    for (subi in 1:nsubjects) {
+      gm <- ctModel(LAMBDA = diag(1), Tpoints = Tpoints, DRIFT = matrix(-.5),
+                    T0MEANS = matrix(4), CINT = matrix(par[subi]),
+                    DIFFUSION = matrix(2), T0VAR = matrix(2),
+                    MANIFESTVAR = matrix(2))
+      d <- suppressMessages(ctGenerate(gm, n.subjects = 1, burnin = 10,
+                                       wide = FALSE, dtmean = dt))
+      if (subi == 1) dat <- cbind(subi, d) else dat <- rbind(dat, cbind(subi, d))
+    }
+
+    colnames(dat)[1] <- "id"
+
+    cm <- ctModel(LAMBDA = diag(1), type = "stanct", CINT = matrix("cint"),
+                  MANIFESTMEANS = matrix(0))
+    cm$pars$indvarying <- FALSE
+
+    dm <- ctModel(LAMBDA = diag(1), type = "standt", CINT = matrix("cint"),
+                  MANIFESTMEANS = matrix(0))
+    dm$pars$indvarying <- FALSE
+
+    for (m in c("cm", "dm")) {
+      argslist <- list(ml = list(datalong = dat, ctstanmodel = get(m), optimize = TRUE,
+                                 nlcontrol = list(), verbose = 0,
+                                 optimcontrol = list(plotsgd = F, estonly = F, stochastic = F),
+                                 savescores = F, nopriors = T),
+                       mlnl = list(datalong = dat, ctstanmodel = get(m), optimize = TRUE,
+                                   nlcontrol = list(nldynamics = TRUE, nlmeasurement = TRUE), verbose = 0,
+                                   optimcontrol = list(plotsgd = F, estonly = F, stochastic = F),
+                                   savescores = F, nopriors = T))
+
+      for (argi in names(argslist)) {
+        f <- do.call(ctStanFit, argslist[[argi]])
+        if (is.null(s[[argi]])) s[[argi]] <- list()
+        s[[argi]][[m]] <- summary(f, parmatrices = TRUE)
       }
-      
-      colnames(dat)[1]='id'
-      
-      cm <- ctModel(LAMBDA=diag(1), type='stanct',
-        CINT=matrix('cint'),
-        MANIFESTMEANS = matrix(0))
-      
-      cm$pars$indvarying <- FALSE
-      
-      dm <- ctModel(LAMBDA=diag(1), type='standt',
-        CINT=matrix('cint'),
-        MANIFESTMEANS = matrix(0))
-      
-      dm$pars$indvarying <- FALSE
-      
-      for(m in c('cm','dm')){
-        argslist <- list(ml=list(datalong = dat,ctstanmodel = get(m),optimize=TRUE, nlcontrol=list(),
-          verbose=0,optimcontrol=list(plotsgd=F,estonly=F,stochastic=F),savescores = F,nopriors=T)
-          ,mlnl=list(datalong = dat,ctstanmodel = get(m),optimize=TRUE, nlcontrol=list(nldynamics=TRUE,nlmeasurement=TRUE),
-            verbose=0,optimcontrol=list(plotsgd=F,estonly=F,stochastic=F),savescores = F,nopriors=T)
-        )
-        
-        
-        for(argi in names(argslist)){
-          f = do.call(ctStanFit,argslist[[argi]])
-          if(is.null(s[[argi]])) s[[argi]] = list()
-          s[[argi]][[m]] <- summary(f,parmatrices=TRUE)
+    }
+    dtpars <- lapply(s, function(argi) {
+                         ct <- argi$cm$parmatrices[c(grep("(^dt|^asym)", rownames(argi$cm$parmatrices)),
+                                                     which(rownames(argi$cm$parmatrices) %in%
+                                                           c("MANIFESTVAR", "LAMBDA", "T0MEANS", "T0VAR"))
+                                                     ), c("Mean", "Sd")]
+                         dt <- argi$dm$parmatrices[c(grep("(^asym)", rownames(argi$dm$parmatrices)),
+                                                     which(rownames(argi$dm$parmatrices) %in%
+                                                           c("DRIFT", "CINT", "DIFFUSIONcov", "MANIFESTVAR",
+                                                             "LAMBDA", "T0MEANS", "T0VAR"))),
+                                                   c("Mean", "Sd")]
+                         t <- paste0("dt", rownames(dt)[rownames(dt) %in% c("DRIFT", "CINT", "DIFFUSIONcov")])
+                         rownames(dt)[rownames(dt) %in% c("DRIFT", "CINT", "DIFFUSIONcov")] <- t
+                         rownames(dt)[rownames(dt) %in% c("dtDIFFUSIONcov")] <- "dtDIFFUSION"
+                         list(ct = ct, dt = dt)
+    })
+    dtpars <- unlist(dtpars, recursive = FALSE)
+    dtpars <- lapply(dtpars, function(x) x[order(rownames(x)), ])
+
+    for (ri in 1:nrow(dtpars$ml.ct)) {
+      for (ci in 1:ncol(dtpars$ml.ct)) {
+        par <- sapply(dtpars, function(y) y[ri, ci])
+        for (dimi in 2:length(par)) {
+          expect_equivalent(par[dimi], par[dimi - 1], tol = ifelse(ci == 1, 1e-1, 5e-1))
         }
       }
-      dtpars=lapply(s, function(argi) {
-        ct=argi$cm$parmatrices[c(grep('(^dt|^asym)',rownames(argi$cm$parmatrices)),
-          which(rownames(argi$cm$parmatrices) %in% c('MANIFESTVAR','LAMBDA','T0MEANS','T0VAR'))),c('Mean','Sd')]
-        dt=argi$dm$parmatrices[c(grep('(^asym)',rownames(argi$dm$parmatrices)),
-          which(rownames(argi$dm$parmatrices) %in% c('DRIFT','CINT','DIFFUSIONcov','MANIFESTVAR','LAMBDA','T0MEANS','T0VAR'))),c('Mean','Sd')]
-        rownames(dt)[rownames(dt) %in% c('DRIFT','CINT','DIFFUSIONcov')] <- paste0('dt',rownames(dt)[rownames(dt) %in% c('DRIFT','CINT','DIFFUSIONcov')])
-        rownames(dt)[rownames(dt) %in% c('dtDIFFUSIONcov')] <- 'dtDIFFUSION'
-        list(ct=ct,dt=dt)
-      }
-      )
-      dtpars=unlist(dtpars,recursive = FALSE)
-      dtpars=lapply(dtpars,function(x) x[order(rownames(x)),])
-      
-      for(ri in 1:nrow(dtpars$ml.ct)){
-        for(ci in 1:ncol(dtpars$ml.ct)){
-          par=sapply(dtpars, function(y) y[ri,ci])
-          for(dimi in 2:length(par)){
-            expect_equivalent(par[dimi],par[dimi-1],tol=ifelse(ci==1,1e-1,5e-1))
-          }
-        }}
-      
-      
-      ll=unlist(lapply(s, function(argi) lapply(argi, function(m) m$logprob)))
-      
-      for(dimi in 2:length(ll)){
-        expect_equivalent(ll[dimi],ll[dimi-1],tol=1e-3)
-      }
-      
-      
-    } #end no heterogeneity
-      
-      
-    )
+    }
+
+    ll <- unlist(lapply(s, function(argi) lapply(argi, function(m) m$logprob)))
+    for (dimi in 2:length(ll)) {
+      expect_equivalent(ll[dimi], ll[dimi - 1], tol = 1e-3)
+    }
+  }) #end no heterogeneity
 }
--- tests/testthat/test-nonlinearVlinear.R
+++ tests/testthat/test-nonlinearVlinear.R
@@ -1,54 +1,67 @@
-if( .Machine$sizeof.pointer != 4){
-library(ctsem)
-library(testthat)
-set.seed(1)
+if (.Machine$sizeof.pointer != 4) {
+  library(ctsem)
+  library(testthat)
+  set.seed(1)
 
-context("nonlinearcheck") 
+  context("nonlinearcheck")
 
-test_that("simplenonlinearcheck", { 
-sunspots<-sunspot.year
- sunspots<-sunspots[50: (length(sunspots) - (1988-1924))]
- id <- 1
- time <- 1749:1924
-datalong <- cbind(id, time, sunspots)
+  test_that("simplenonlinearcheck", {
+    sunspots <- sunspot.year
+    sunspots <- sunspots[50:(length(sunspots) - (1988 - 1924))]
+    id <- 1
+    time <- 1749:1924
+    datalong <- cbind(id, time, sunspots)
 
-#setup model
- ssmodel <- ctModel(type='stanct', n.latent=2, n.manifest=1, 
-    # n.TDpred = 1,
-  manifestNames='sunspots', 
-  latentNames=c('ss_level', 'ss_velocity'),
-   LAMBDA=matrix(c( 1, 'ma1'), nrow=1, ncol=2),
-   DRIFT=matrix(c(0, 'a21|-log1p(exp(param))', 1, 'a22'), nrow=2, ncol=2),
-   TDPREDEFFECT=matrix(c('tdeffect',0),2),
-   MANIFESTMEANS=matrix(c('mm|param*10+44'), nrow=1, ncol=1),
-   MANIFESTVAR=diag(0,1),
-   T0VAR=matrix(c(1,0,0,1), nrow=2, ncol=2), #Because single subject
-   DIFFUSION=matrix(c(0, 0, 0, 'diff'), ncol=2, nrow=2))
+    #setup model
+    ssmodel <- ctModel(type = "stanct", n.latent = 2, n.manifest = 1,
+                       # n.TDpred = 1,
+                       manifestNames = "sunspots",
+                       latentNames = c("ss_level", "ss_velocity"),
+                       LAMBDA = matrix(c(1, "ma1"), nrow = 1, ncol = 2),
+                       DRIFT = matrix(c(0, "a21|-log1p(exp(param))", 1, "a22"), nrow = 2, ncol = 2),
+                       TDPREDEFFECT = matrix(c("tdeffect", 0), 2),
+                       MANIFESTMEANS = matrix(c("mm|param*10+44"), nrow = 1, ncol = 1),
+                       MANIFESTVAR = diag(0, 1),
+                       T0VAR = matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2),
+                       #Because single subject
+                       DIFFUSION = matrix(c(0, 0, 0, "diff"), ncol = 2, nrow = 2))
 
- #td preds for testing only -- no real effect
- TD1 <- 0
- datalong <- cbind(datalong,TD1)
- datalong[seq(10,150,10),'TD1'] = 1
+    #td preds for testing only -- no real effect
+    TD1 <- 0
+    datalong <- cbind(datalong, TD1)
+    datalong[seq(10, 150, 10), "TD1"] <- 1
 
-ssfitnl <- ctStanFit(datalong, ssmodel, iter=300, chains=1,optimize=T,verbose=0,maxtimestep = .3,
-  nlcontrol=list(nldynamics=TRUE),optimcontrol = list(finishsamples=10),nopriors=TRUE,deoptim=FALSE)
-ssfitl <- ctStanFit(datalong, ssmodel, iter=300, chains=1,optimize=T,verbose=0,
-  nlcontrol=list(nldynamics=FALSE),optimcontrol = list(finishsamples=10,deoptim=FALSE),nopriors=TRUE)
+    ssfitnl <- ctStanFit(datalong, ssmodel, iter = 300, chains = 1,
+                         optimize = T, verbose = 0, maxtimestep = .3,
+                         nlcontrol = list(nldynamics = TRUE),
+                         optimcontrol = list(finishsamples = 10),
+                         nopriors = TRUE, deoptim = FALSE)
+    ssfitl <- ctStanFit(datalong, ssmodel, iter = 300, chains = 1,
+                        optimize = T, verbose = 0,
+                        nlcontrol = list(nldynamics = FALSE),
+                        optimcontrol = list(finishsamples = 10, deoptim = FALSE),  # FIXME: wrong param position?
+                        nopriors = TRUE)
 
-ssfitnlm <- ctStanFit(datalong, ssmodel, iter=300, chains=1,optimize=T,verbose=0,maxtimestep = 2,fit=T,
-   # forcerecompile=T,
-  nlcontrol=list(nldynamics=T,nlmeasurement=T),optimcontrol = list(finishsamples=10),nopriors=TRUE,deoptim=FALSE)
+    ssfitnlm <- ctStanFit(datalong, ssmodel, iter = 300, chains = 1,
+                          optimize = T, verbose = 0, maxtimestep = 2,
+                          fit = T,
+                          # forcerecompile=T,
+                          nlcontrol = list(nldynamics = T, nlmeasurement = T),
+                          optimcontrol = list(finishsamples = 10),
+                          nopriors = TRUE, deoptim = FALSE)
 
-#output
-# snl=summary(ssfitnl)
-# snlm=summary(ssfitnlm)
-# sl=summary(ssfitl)
+    #output
+    # snl=summary(ssfitnl)
+    # snlm=summary(ssfitnlm)
+    # sl=summary(ssfitl)
 
-# expect_equal(snl$popmeans[,'mean'], sl$popmeans[,'mean'])
-expect_equal(round(ssfitnl$stanfit$transformedpars_old[grep('pop_',rownames(ssfitnl$stanfit$transformedpars_old)),'mean'],3),
-round(ssfitl$stanfit$transformedpars_old[grep('pop_',rownames(ssfitnl$stanfit$transformedpars_old)),'mean'],3),tol=.01)
+    # expect_equal(snl$popmeans[,'mean'], sl$popmeans[,'mean'])
+    expect_equal(round(ssfitnl$stanfit$transformedpars_old[grep("pop_", rownames(ssfitnl$stanfit$transformedpars_old)), "mean"], 3),
+                 round(ssfitl$stanfit$transformedpars_old[grep("pop_", rownames(ssfitnl$stanfit$transformedpars_old)), "mean"], 3),
+                 tol = .01)
 
-expect_equal(round(ssfitnlm$stanfit$transformedpars_old[grep('pop_',rownames(ssfitnl$stanfit$transformedpars_old)),'mean'],3),
-round(ssfitl$stanfit$transformedpars_old[grep('pop_',rownames(ssfitnl$stanfit$transformedpars_old)),'mean'],3),tol=.01)
-})
+    expect_equal(round(ssfitnlm$stanfit$transformedpars_old[grep("pop_", rownames(ssfitnl$stanfit$transformedpars_old)), "mean"], 3),
+                 round(ssfitl$stanfit$transformedpars_old[grep("pop_", rownames(ssfitnl$stanfit$transformedpars_old)), "mean"], 3),
+                 tol = .01)
+  })
 }
